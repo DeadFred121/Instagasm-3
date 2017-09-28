1fd6643356ca2f96f1684dc827717d06eb9ee1e9
DeadFred <f.cliff1@hotmail.com>
2017-09-28 14:35:16 +1000

    Devise fully implemented

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170928032109) do
+ActiveRecord::Schema.define(version: 20170928042442) do
 
   create_table "posts", force: :cascade do |t|
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "comment"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
